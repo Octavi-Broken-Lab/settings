2c3a352ba46265d8c1dcba2839ced0980004296e
Dixzz <vishusatyam36@gmail.com>
2020-10-30 14:35:26 +0000

    Minimial Design * also check for official before adding icon

--- src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
@@ -72,6 +72,7 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
             final Preference multiSimPreference = createNewPreference(screen.getContext());
             multiSimPreference.setOrder(imeiPreferenceOrder + simSlotNumber);
             multiSimPreference.setKey(getPreferenceKey() + simSlotNumber);
+	    multiSimPreference.setIconSpaceReserved(false);
             screen.addPreference(multiSimPreference);
             mPreferenceList.add(multiSimPreference);
             updatePreference(multiSimPreference, simSlotNumber);
